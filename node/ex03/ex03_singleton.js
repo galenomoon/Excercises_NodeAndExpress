6f7a5076b00d06db6137fda36cccc2d10f81a952
galeno <gui.galenocover@gmail.com>
2022-01-29 12:09:48 -0300

    ex03

---
+++ node/ex03/ex03_singleton.js
@@ -0,0 +1,7 @@
+let numero = 1
+
+function exibirPróximo() {
+    console.log(numero++)
+}
+
+module.exports = { exibirPróximo }
---
+++ node/ex03/ex03_teste.js
@@ -0,0 +1,7 @@
+const s1 = require('./ex03_singleton')
+const s2 = require('./ex03_singleton')
+
+s1.exibirPróximo()
+s2.exibirPróximo()
+s1.exibirPróximo()
+s2.exibirPróximo()
